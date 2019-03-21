240307c4bdcdfce60082504047418f8894e16b70
Thiago de Souza <email@thiagodesouza.com.br>
2019-03-20 22:22:15 -0300

    v4.2.0 - Add configuration to log error: sentry

--- app/Exceptions/Handler.js
+++ app/Exceptions/Handler.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const Raven = require('raven')
+const Config = use('Config')
+
 const Env = use('Env')
 const Youch = use('youch')
 const BaseExceptionHandler = use('BaseExceptionHandler')
@@ -48,7 +51,10 @@ class ExceptionHandler extends BaseExceptionHandler {
    * @return {void}
    */
   async report (error, { request }) {
-    console.log(error)
+    if (error.name !== 'ValidationException') {
+      Raven.config(Config.get('services.sentry.dsn'))
+      Raven.captureException(error)
+    }
   }
 }
 
